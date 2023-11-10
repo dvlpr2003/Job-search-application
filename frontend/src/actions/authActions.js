bfcab6ab304763907c7a1fb272167790859b6a5e
RAJA K <gayathrigaya698@gmail.com>
2023-11-10 10:56:20 +0530

    Update authActions.js

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -17,7 +17,7 @@ export const registerUser = (userData, history) => dispatch => {
         //         type: GET_ERRORS,
         //         payload: err.response.data,
         //     })
-    );
+    // );
 };
 
 // Login - get user token
